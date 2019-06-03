a84c9c101a33361d59daa910e789912514d3133b
zhixia <reamyx@126.com>
2019-06-03 11:44:04 +0800

    修正dante外部认证过程参数引用

--- proxydante3/skdaccountck.sh
+++ proxydante3/skdaccountck.sh
@@ -7,17 +7,7 @@ exec 4>&1; ECHO(){ echo "${@}" >&4; }; exec 3<>"/dev/null"; exec 0<&3;exec 1>&3;
 #标准输出首行提供密码,次行输出可选的描述信息
 #输出字串中的'\0'被视作换行符'\n'处理,非0状态码指示认证失败
 
-#PPPD程序(expandpwd插件)和SOCKD服务调用参数解释:
-# { "method": "CHAP", "usercnm": "abc000", "usercpw": "xxx", "ipparm": "xxx",
-#   "srvpid": "2233", "srvname": "PPTP", "asessid": "0FA5A89B7E", ... }
-#属性解释:
-#  method    认证方法: "PAP", "CHAP", "SKDPW"
-#  usercnm   对端认证名称
-#  usercpw   提交密码明文,仅PAP方法时适用
-#  ipparm    pppd程序之ipparm选项值(仅PPPD)
-#  srvpid    服务主进程PID
-#  asessid   认证关联的会话ID
-#  srvname   服务名称
+#SOCKD服务调用参数解释: $1:用户名称 $2:用户提交明文密码 $3:sockd服务主进程ID
 
 #proxy集群etcd目录规划
 # /proxyauth/LinesCommon/*    #常规认证卡密,低优先适用所有线路
@@ -25,7 +15,7 @@ exec 4>&1; ECHO(){ echo "${@}" >&4; }; exec 3<>"/dev/null"; exec 0<&3;exec 1>&3;
 # 认证数据: '{"passwd":"abc000","limit":2,"expire":"","type":""}'
 
 #用户名称
-UNM="$( echo "$1" | jq -rcM ".usercnm|strings" )"
+UNM="$1"
 
 #测试数据
 [ "$UNM" == "vtest" ] && { ECHO "2019"; ECHO "vtest-2019-ok"; exit 0; }
@@ -33,7 +23,7 @@ UNM="$( echo "$1" | jq -rcM ".usercnm|strings" )"
 #优先从缓存查找用户账户信息,失败时从远程拉取并更新到缓存表
 PWDCC="./pswdcc.db"
 UPW="$( sqlite3 --cmd ".timeout 3000" "$PWDCC" "
-SELECT pswd FROM usinfo WHERE name=='$UNM';" )"
+    SELECT pswd FROM usinfo WHERE name=='$UNM';" )"
 
 #缓存未命中时从远程拉取用户信息
 [ -z "$UPW" ] && {
